cc17acd874be9e779155cb5f199d2dbbc7b47d75
NatalieLouise91 <natalie936@hotmail.com>
2022-11-14 13:46:32 +1000

    Updated status codes for user controller methods, updated auth routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-            render json: @user, status: :created
+            render json: @user, status: 201
         else
             render json: { errors: @user.errors.full_messages }, 
             status: :unprocessable_entity
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,7 @@ Rails.application.routes.draw do
   resources :cards, only: [:index, :show, :create, :update, :destroy]
 
   resources :users, param: :_username
+  post '/auth/register', to: 'users#create'
   post '/auth/login', to: 'authentication#login'
   get '/*a', to: 'application#not_found'
-  
-  get '/current_user' => 'authentication#index'
 end
